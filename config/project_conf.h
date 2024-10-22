83d3ffa63d07c591a4890a319e97be66b33d2e99
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-22 18:40:08 -0300

    attt

---
+++ config/project_conf.h
@@ -0,0 +1,7 @@
+#define LUA_FOLDER  "build/stage3/lua"
+#define CONF_FOLDER "config"
+#define OUTPUT_LUA "build/stage3/c/globals.lua_code.c"
+#define LUA_VAR_NAME  "lua_code"
+#define LUA_START_POINT  "main.lua"
+#define FINAL_OUPTUT_LINUX  "build.out"
+#define FINAL_OUPTUT_WINDOWS  "../build.exe"
